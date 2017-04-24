c06d65382becfbbb51a2f6cbd37455ecf85c9828
chunguhn <cu_kim@interpark.com>
2017-04-24 11:23:28 +0900

    asd

---
+++ chung/inc/dbInfo.php
@@ -0,0 +1,11 @@
+<?php
+    define('_HOST_', '10.20.4.62');
+    define('__ID__', 'root');
+    define('__PWD__', 'phpStudy1!');
+    define('__DB__', 'php_study');
+?>
+<h3>Connection Info</h3>
+<p><code>define('_HOST_', '10.20.4.62');</code></p>
+<p><code>define('__ID__', 'root');</code></p>
+<p><code>define('__PWD__', 'phpStudy1!');</code></p>
+<p><code>define('__DB__', 'php_study');</code></p>
